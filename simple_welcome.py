d5a138318bb4e93e389e4c1f52fc9d326806812a
Macholu <luisacruzjr31@gmail.com>
2025-06-15 15:10:53 -0400

    Add files via upload

---
+++ simple_welcome.py
@@ -0,0 +1,10 @@
+# simple_welcome.py
+def welcome(name):
+    print(f"Welcome to your AI journey, {name}!")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    welcome(user_name)
+# simple_welcome.py
+def welcome(name):
+    print(f"Welcome to your AI journey, {name}!")
